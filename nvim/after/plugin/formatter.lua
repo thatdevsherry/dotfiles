16f279f72c98032383a07a765baa811703f6f0ba
Shehriyar Qureshi <thatdevsherry@gmail.com>
2023-11-16 11:38:21 +0500

    nvim stuff

--- nvim/after/plugin/formatter.lua
+++ nvim/after/plugin/formatter.lua
@@ -1,15 +1,24 @@
-require('formatter').setup({
-    logging = true,
-    log_level = vim.log.levels.WARN,
+require("formatter").setup({
+	logging = true,
+	log_level = vim.log.levels.WARN,
 
-    filetype = {
-        python = {
-            require('formatter.filetypes.python').black,
-        },
-    },
+	filetype = {
+		python = {
+			require("formatter.filetypes.python").black,
+		},
+		javascript = {
+			require("formatter.filetypes.javascript").eslint_d,
+		},
+		typescript = {
+			require("formatter.filetypes.typescript").eslint_d,
+		},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+	},
 
-    -- Any filetype
-    ['*'] = {
-        require('formatter.filetypes.any').remove_trailing_whitespace
-    }
+	-- Any filetype
+	["*"] = {
+		require("formatter.filetypes.any").remove_trailing_whitespace,
+	},
 })
--- nvim/after/plugin/linter.lua
+++ nvim/after/plugin/linter.lua
@@ -1,4 +1,12 @@
-require('lint').linters_by_ft = {
-    python = { "mypy", "pylint" },
-    markdown = { 'markdownlint' }
+local linter = require("lint")
+
+linter.linters_by_ft = {
+	python = { "mypy", "pylint" },
+	markdown = { "markdownlint" },
 }
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+	callback = function()
+		linter.try_lint()
+	end,
+})
