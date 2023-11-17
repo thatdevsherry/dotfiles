407709d453cfdce3e7ff593c3864b5d0acabaf13
Shehriyar Qureshi <thatdevsherry@gmail.com>
2023-11-17 14:12:46 +0500

    moar formatters

--- nvim/after/plugin/formatter.lua
+++ nvim/after/plugin/formatter.lua
@@ -3,15 +3,40 @@ require("formatter").setup({
 	log_level = vim.log.levels.WARN,
 
 	filetype = {
+		c = {
+			require("formatter.filetypes.c").clangformat,
+		},
 		python = {
-			require("formatter.filetypes.python").black,
+            -- Keep sorts before so formatter can work after sorting
 			require("formatter.filetypes.python").isort,
+			require("formatter.filetypes.python").black,
 		},
 		javascript = {
-			require("formatter.filetypes.javascript").eslint_d,
+			require("formatter.filetypes.javascript").prettiereslint,
+		},
+		javascriptreact = {
+			require("formatter.filetypes.javascriptreact").prettiereslint,
 		},
 		typescript = {
-			require("formatter.filetypes.typescript").eslint_d,
+			require("formatter.filetypes.typescript").prettiereslint,
+		},
+		typescriptreact = {
+			require("formatter.filetypes.typescriptreact").prettiereslint,
+		},
+		json = {
+			require("formatter.filetypes.json").prettier,
+		},
+		html = {
+			require("formatter.filetypes.html").prettier,
+		},
+		graphql = {
+			require("formatter.filetypes.graphql").prettier,
+		},
+		rust = {
+			require("formatter.filetypes.rust").rustfmt,
+		},
+		terraform = {
+			require("formatter.filetypes.terraform").terraformfmt,
 		},
 		lua = {
 			require("formatter.filetypes.lua").stylua,
