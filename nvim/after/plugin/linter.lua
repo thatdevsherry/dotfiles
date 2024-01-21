cb3bbe23d333b705b044112a6ab579a5a574e318
Shehriyar Qureshi <m.shehriyar@abysssolutions.com.au>
2024-01-21 16:11:30 +0500

    something

--- nvim/after/plugin/linter.lua
+++ nvim/after/plugin/linter.lua
@@ -1,12 +1,29 @@
 local linter = require("lint")
+local mypy = linter.linters.mypy
+local pylint = linter.linters.pylint
+
+mypy.args = {
+	-- Use virtualenv mypy and not mason one
+	"--python-executable",
+	"python",
+	-- END
+	"--show-column-numbers",
+	"--show-error-end",
+	"--hide-error-codes",
+	"--hide-error-context",
+	"--no-color-output",
+	"--no-error-summary",
+	"--no-pretty",
+}
+pylint.cmd = "pylint"
 
 linter.linters_by_ft = {
-	python = { "mypy", "pylint" },
+	python = { "pylint", "mypy" },
 	markdown = { "markdownlint" },
 }
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
 	callback = function()
-		linter.try_lint()
+		require("lint").try_lint()
 	end,
 })
